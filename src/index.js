0a9b4710903418415a00d13d954119588d7349eb
A Sadavoy <sadavoya@gmail.com>
2020-10-31 11:57:51 -0400

    Set up Auth0

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//olsauth.us.auth0.com
+//pNgPsAjBaRSvjvnDSVa4zi7IPzX3v2dS
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="olsauth.us.auth0.com"
+      clientId="pNgPsAjBaRSvjvnDSVa4zi7IPzX3v2dS"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
