e496af868ba5cc8ef96f2e5ed091eea17e1e52a2
A Sadavoy <sadavoya@gmail.com>
2020-10-31 13:02:31 -0400

    OOPS: Update PrivateRoute: * Get Auth0 data * Redirect to Login when not logged in * Continue on to children route if logged in

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
+// By default, let's redirect to the login page
 const defaultRedirectPath = "/login";
 const PrivateRoute = ({ children, ...rest }) => {
+  // Get login state from Auth0
   const { isAuthenticated, user } = useAuth0();
+  // isUser indicates whether or not we are logged in
   const isUser = isAuthenticated && user;
 
   return (
     <Route
       {...rest}
       render={() => {
+        // If we are logged in, continue on to the child route
+        // if we are NOT logged int, redirect to the defaultRedirectPath
         return isUser ? children : <Redirect to={defaultRedirectPath} />;
       }}
     ></Route>
