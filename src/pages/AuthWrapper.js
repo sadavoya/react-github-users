b452ba155ce751f6515e0079bef4a4ba35442869
A Sadavoy <sadavoya@gmail.com>
2020-10-31 13:05:25 -0400

    Update AuthWrapper: * Handle isLoading * Handle error

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import loadingGif from '../images/preloader.gif';
-import styled from 'styled-components';
-function AuthWrapper() {
-  return <h2>authwrapper component</h2>;
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import loadingGif from "../images/preloader.gif";
+import styled from "styled-components";
+function AuthWrapper({ children }) {
+  // Get the Auth0 data
+  const { isLoading, error } = useAuth0();
+
+  // If we are still loading...
+  if (isLoading) {
+    // ...show a spinner
+    return (
+      <Wrapper>
+        <img src={loadingGif} alt="spinner" />
+      </Wrapper>
+    );
+  }
+  // If there is an error...
+  if (error) {
+    // ...display an error
+    return (
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    );
+  }
+  // Otherwise, display the children
+  return <>{children}</>;
 }
 
 const Wrapper = styled.section`
