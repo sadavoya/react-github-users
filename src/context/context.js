0dc1390e20949e3c6a8fc4ed87cb2871697f7483
A Sadavoy <sadavoya@gmail.com>
2020-10-31 11:28:40 -0400

    Update context: * set isLoading to true when loading * set isLoading to false when not loading, including after an error * get supplemental data urls from the user data and load those urls * use Promise.allSettled to load supplemental data in parallel

--- src/context/context.js
+++ src/context/context.js
@@ -4,38 +4,69 @@ import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
 
+const DEBUG = false;
+
+const reposPerPage = 100;
+const followersPerPage = 100;
 const rootUrl = "https://api.github.com";
-const userUrl = `${rootUrl}/users`;
+const userUrlBase = `${rootUrl}/users`;
+const reposUrlSuffix = `?per_page=${reposPerPage}`;
+const followersUrlSuffix = `?per_page=${followersPerPage}`;
 
 const GithubContext = React.createContext();
 const GithubProvider = ({ children }) => {
+  const [requests, setRequests] = useState({});
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   // loading
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   // error
   const [error, setError] = useState({ show: false, msg: "" });
+
+  // The search function
   const searchGithubUser = async (user) => {
+    // userUrl
+    const userUrl = `${userUrlBase}/${user}`;
     // toggle error
     toggleError(false, "");
-    // setLoading(true)
-    const response = await axios(`${userUrl}/${user}`).catch((err) =>
-      console.log(err)
-    );
-    console.log(response);
+    setIsLoading(true);
+    const response = await axios(userUrl).catch((err) => {
+      setIsLoading(false);
+      console.log(err);
+    });
+    DEBUG && console.log(response);
     if (response) {
+      let { repos_url, followers_url } = response.data;
+      repos_url += reposUrlSuffix;
+      followers_url += followersUrlSuffix;
+
+      DEBUG && console.log(repos_url, followers_url);
       setGithubUser(response.data);
+      await Promise.allSettled([axios(followers_url), axios(repos_url)])
+        .then((results) => {
+          const [followers, repos] = results;
+          const successStatus = "fulfilled";
+          if (repos.status === successStatus) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === successStatus) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
+
       // more logic here
     } else {
       toggleError(true, "there is no user with that username");
     }
+    checkRequests();
+    setIsLoading(false);
   };
-  // request loading
-  const [requests, setRequests] = useState({});
-  // const [isLoading, setIsLoading] = useState(false);
-  // check rate
+
+  // The function to determine check limit and remaining
   const checkRequests = () => {
+    setIsLoading(true);
     axios(`${rootUrl}/rate_limit`)
       .then(({ data }) => {
         let { rate } = data;
@@ -44,8 +75,12 @@ const GithubProvider = ({ children }) => {
         if (rate.remaining === 0) {
           toggleError(true, "sorry, you have exceeded your hourly rate limit");
         }
+        setIsLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setIsLoading(false);
+        DEBUG && console.log(err);
+      });
   };
   function toggleError(show, msg) {
     setError({ show, msg });
@@ -61,6 +96,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
